3345b46ad6ed8af3758c36a43d4698721987df38
SangDo-Kim <167182723+SangDo-Kim@users.noreply.github.com>
2024-05-21 02:35:11 +0900

    V1.3

--- text_converter.py
+++ text_converter.py
@@ -1,4 +1,4 @@
-"""# Text Converter V1.21 for programmers (GUI in Korean)
+"""# Text Converter V1.3 (GUI in Korean)
 Written by SangDo_Kim
 This program converts various text to a form frequently used in Python programmings, like list, dictionary,
 unicode escape sequence, white space characters.
@@ -51,47 +51,52 @@ V1.2
 
 V1.21
 - Changes in string_to_tree.py (Put functions into class, bug fixes)
+
+V1.3
+- Added JAV product code extract feature.
 """
 import json
 
 from PySide6.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
 from text_converter_ui import Ui_Form
-from string_to_tree import build_tree
+from sangdo_mod.string_to_tree import build_tree
+from sangdo_mod.JAV_prod_code import JAV_prod_code, NoCodeError
+
 
 class MainWindow(QWidget, Ui_Form):
     def __init__(self, *args, obj=None, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Initialize
         self.setupUi(self)
-
-        self.setWindowTitle("텍스트 변환기 V1.21")
-        self.plainTextEdit.setTabStopDistance(20)
-        self.plainTextEdit_2.setTabStopDistance(20)
+        self.setWindowTitle("텍스트 변환기 V1.3")
+        self.plainTextEdit_source.setTabStopDistance(20)
+        self.plainTextEdit_target.setTabStopDistance(20)
         self.label_status.setText("개발자: SangDo_Kim, 블로그: sangdo-kim.blogspot.com")
+        self.plainTextEdit_source.setFocus()
 
-        self.plainTextEdit.setFocus()
+        # Attributes
+        self.target_original = ""
+
+        # Connect
         self.pushButton_list.clicked.connect(self.convert_to_list)
         self.pushButton_clipboard.clicked.connect(self.copy_to_clipboard)
         self.pushButton_unicode.clicked.connect(self.convert_unicode)
         self.pushButton_clear.clicked.connect(self.clear_source)
         self.pushButton_white_space.clicked.connect(self.convert_white_space)
         self.pushButton_dict.clicked.connect(self.convert_to_dict)
+        self.pushButton_prod_code.clicked.connect(self.convert_to_prod_code)
+        self.pushButton_add.clicked.connect(self.add_pre_suf)
 
     def convert_to_list(self):
-        text = self.plainTextEdit.toPlainText()
+        text = self.plainTextEdit_source.toPlainText()
         list1 = self.string_to_nested_list(text)
         if self.checkBox_tab.isChecked():
-            if self.checkBox_number.isChecked():
-                text_mod = json.dumps(list1, indent="\t", separators=(",", ":"))
-            else:
-                text_mod = json.dumps(list1, indent="\t")
+            text_mod = json.dumps(list1, ensure_ascii=False, indent="\t", separators=(",", ":"))
         else:
-            if self.checkBox_number.isChecked():
-                json_string = json.dumps(data, indent="\t", separators=(",", ":"))
-            else:
-                text_mod = json.dumps(list1)
+            text_mod = json.dumps(list1, ensure_ascii=False)
 
-        self.plainTextEdit_2.setPlainText(text_mod)
-        self.label_status.setText("리스트로 변환했습니다.")
+        self.set_target(text_mod)
 
     def string_to_nested_list(self, string):
         list_split = string.split("\n")
@@ -112,15 +117,14 @@ class MainWindow(QWidget, Ui_Form):
 
     def copy_to_clipboard(self):
         clipboard = QApplication.clipboard()
-        clipboard.setText(self.plainTextEdit_2.toPlainText())
+        clipboard.setText(self.plainTextEdit_target.toPlainText())
         # QMessageBox.information(self, "정보", "클립보드로 복사되었습니다.")
         self.label_status.setText("클립보드로 복사했습니다.")
 
     def convert_unicode(self):
-        text_unicode = self.plainTextEdit.toPlainText()
+        text_unicode = self.plainTextEdit_source.toPlainText()
         text = self.decode_unicode_escape(text_unicode)
-        self.plainTextEdit_2.setPlainText(text)
-        self.label_status.setText("유니코드 이스케이프 시퀀스를 문자로 변환했습니다.")
+        self.set_target(text)
 
     def decode_unicode_escape(self, text):
         # Define escape sequences to exclude
@@ -140,33 +144,65 @@ class MainWindow(QWidget, Ui_Form):
         return text.encode().decode('unicode_escape')
 
     def clear_source(self):
-        self.plainTextEdit.clear()
+        self.plainTextEdit_source.clear()
         self.label_status.setText("원본을 지웠습니다.")
 
     def convert_white_space(self):
-        text = self.plainTextEdit.toPlainText()
-        text_mod = json.dumps(text)
+        text = self.plainTextEdit_source.toPlainText()
+        text_mod = json.dumps(text, ensure_ascii=False)
         # list1 = text.replace("\n", '", "')
         # list1 = f'["{list1}"]'
-        self.plainTextEdit_2.setPlainText(text_mod)
-        self.label_status.setText("리스트로 변환했습니다.")
+        self.set_target(text_mod)
 
     def convert_to_dict(self):
-        text = self.plainTextEdit.toPlainText()
+        text = self.plainTextEdit_source.toPlainText()
         _dict = build_tree.string_to_dict(str(text))
 
         if self.checkBox_tab.isChecked():
-            text_mod = json.dumps(_dict, indent="\t")
+            text_mod = json.dumps(_dict, ensure_ascii=False, indent="\t")
         else:
-            text_mod = json.dumps(_dict)
+            text_mod = json.dumps(_dict, ensure_ascii=False)
         text_mod = text_mod.replace('": null', '": None')
-        self.plainTextEdit_2.setPlainText(text_mod)
+        self.set_target(text_mod)
         if text.find("\t\t\t") >= 0:
             self.label_status.setText("딕셔너리 변환 오류")
             QMessageBox.information(self, "정보", "현재 딕셔너리에 대해서는 3단계 계층 구조까지만 지원합니다.")
         else:
             self.label_status.setText("딕셔너리로 변환했습니다.")
 
+    def convert_to_prod_code(self):
+        source_lines = self.plainTextEdit_source.toPlainText().splitlines()
+        target_text = ""
+        for line in source_lines:
+            try:
+                prod_code = JAV_prod_code.get_prod_code(line)
+            except NoCodeError:
+                continue
+            else:
+                target_text += prod_code + "\n"
+        if target_text.endswith("\n"):
+            target_text = target_text[:-1]
+
+        self.set_target(target_text)
+
+    def add_pre_suf(self):
+        target_lines = self.target_original.splitlines()
+        new_target_text = ""
+        for line in target_lines:
+            new_line = self.lineEdit_add_pre.text() + line + self.lineEdit_add_suf.text()
+            new_target_text += new_line + "\n"
+        if new_target_text.endswith("\n"):
+            new_target_text = new_target_text[:-1]
+        self.plainTextEdit_target.setPlainText(new_target_text)
+
+    def set_target(self, target_contents):
+        self.target_original = target_contents
+        self.plainTextEdit_target.setPlainText(target_contents)
+        if len(self.target_original) <= 0:
+            self.label_status.setText(f"변환할 내용이 없습니다.")
+        else:
+            target_line_no = self.target_original.count("\n") + 1
+            self.label_status.setText(f"내용을 변환했습니다(결과: {target_line_no}줄)")
 
 app = QApplication()
 main_window = MainWindow()
--- text_converter_ui.py
+++ text_converter_ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'text_converter_uiOmzKln.ui'
+## Form generated from reading UI file 'text_converter_uiyfjxvX.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.0
 ##
@@ -16,8 +16,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
-    QLabel, QPlainTextEdit, QPushButton, QSizePolicy,
-    QWidget)
+    QHBoxLayout, QLabel, QLineEdit, QPlainTextEdit,
+    QPushButton, QSizePolicy, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -46,19 +46,19 @@ class Ui_Form(object):
 
         self.label_status = QLabel(Form)
         self.label_status.setObjectName(u"label_status")
-        self.label_status.setEnabled(False)
+        self.label_status.setEnabled(True)
 
         self.gridLayout.addWidget(self.label_status, 4, 0, 1, 2)
 
-        self.plainTextEdit_2 = QPlainTextEdit(Form)
-        self.plainTextEdit_2.setObjectName(u"plainTextEdit_2")
+        self.plainTextEdit_target = QPlainTextEdit(Form)
+        self.plainTextEdit_target.setObjectName(u"plainTextEdit_target")
 
-        self.gridLayout.addWidget(self.plainTextEdit_2, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.plainTextEdit_target, 1, 1, 1, 1)
 
-        self.plainTextEdit = QPlainTextEdit(Form)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit_source = QPlainTextEdit(Form)
+        self.plainTextEdit_source.setObjectName(u"plainTextEdit_source")
 
-        self.gridLayout.addWidget(self.plainTextEdit, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.plainTextEdit_source, 1, 0, 1, 1)
 
         self.groupBox_2 = QGroupBox(Form)
         self.groupBox_2.setObjectName(u"groupBox_2")
@@ -68,6 +68,36 @@ class Ui_Form(object):
         self.pushButton_clipboard.setObjectName(u"pushButton_clipboard")
 
         self.gridLayout_2.addWidget(self.pushButton_clipboard, 0, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_3 = QLabel(self.groupBox_2)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout.addWidget(self.label_3)
+
+        self.lineEdit_add_pre = QLineEdit(self.groupBox_2)
+        self.lineEdit_add_pre.setObjectName(u"lineEdit_add_pre")
+
+        self.horizontalLayout.addWidget(self.lineEdit_add_pre)
+
+        self.label_4 = QLabel(self.groupBox_2)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout.addWidget(self.label_4)
+
+        self.lineEdit_add_suf = QLineEdit(self.groupBox_2)
+        self.lineEdit_add_suf.setObjectName(u"lineEdit_add_suf")
+
+        self.horizontalLayout.addWidget(self.lineEdit_add_suf)
+
+
+        self.gridLayout_2.addLayout(self.horizontalLayout, 2, 0, 1, 3)
+
+        self.pushButton_add = QPushButton(self.groupBox_2)
+        self.pushButton_add.setObjectName(u"pushButton_add")
+
+        self.gridLayout_2.addWidget(self.pushButton_add, 0, 1, 1, 1)
 
 
         self.gridLayout.addWidget(self.groupBox_2, 3, 1, 1, 1)
@@ -94,41 +124,46 @@ class Ui_Form(object):
         self.groupBox.setMaximumSize(QSize(16777215, 16777215))
         self.gridLayout_3 = QGridLayout(self.groupBox)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.checkBox_tab = QCheckBox(self.groupBox)
-        self.checkBox_tab.setObjectName(u"checkBox_tab")
-
-        self.gridLayout_3.addWidget(self.checkBox_tab, 0, 2, 1, 1)
-
         self.pushButton_white_space = QPushButton(self.groupBox)
         self.pushButton_white_space.setObjectName(u"pushButton_white_space")
 
         self.gridLayout_3.addWidget(self.pushButton_white_space, 2, 1, 1, 1)
-
-        self.pushButton_dict = QPushButton(self.groupBox)
-        self.pushButton_dict.setObjectName(u"pushButton_dict")
-
-        self.gridLayout_3.addWidget(self.pushButton_dict, 0, 1, 1, 1)
-
-        self.pushButton_clear = QPushButton(self.groupBox)
-        self.pushButton_clear.setObjectName(u"pushButton_clear")
-
-        self.gridLayout_3.addWidget(self.pushButton_clear, 2, 2, 1, 1)
 
         self.pushButton_unicode = QPushButton(self.groupBox)
         self.pushButton_unicode.setObjectName(u"pushButton_unicode")
 
         self.gridLayout_3.addWidget(self.pushButton_unicode, 2, 0, 1, 1)
 
-        self.pushButton_list = QPushButton(self.groupBox)
-        self.pushButton_list.setObjectName(u"pushButton_list")
+        self.checkBox_tab = QCheckBox(self.groupBox)
+        self.checkBox_tab.setObjectName(u"checkBox_tab")
 
-        self.gridLayout_3.addWidget(self.pushButton_list, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.checkBox_tab, 0, 2, 1, 1)
+
+        self.pushButton_dict = QPushButton(self.groupBox)
+        self.pushButton_dict.setObjectName(u"pushButton_dict")
+
+        self.gridLayout_3.addWidget(self.pushButton_dict, 0, 1, 1, 1)
 
         self.checkBox_number = QCheckBox(self.groupBox)
         self.checkBox_number.setObjectName(u"checkBox_number")
         self.checkBox_number.setEnabled(False)
 
-        self.gridLayout_3.addWidget(self.checkBox_number, 1, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.checkBox_number, 0, 3, 1, 1)
+
+        self.pushButton_list = QPushButton(self.groupBox)
+        self.pushButton_list.setObjectName(u"pushButton_list")
+
+        self.gridLayout_3.addWidget(self.pushButton_list, 0, 0, 1, 1)
+
+        self.pushButton_prod_code = QPushButton(self.groupBox)
+        self.pushButton_prod_code.setObjectName(u"pushButton_prod_code")
+
+        self.gridLayout_3.addWidget(self.pushButton_prod_code, 3, 0, 1, 1)
+
+        self.pushButton_clear = QPushButton(self.groupBox)
+        self.pushButton_clear.setObjectName(u"pushButton_clear")
+
+        self.gridLayout_3.addWidget(self.pushButton_clear, 2, 3, 1, 1)
 
 
         self.gridLayout.addWidget(self.groupBox, 3, 0, 1, 1)
@@ -143,41 +178,48 @@ class Ui_Form(object):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"\ud14d\uc2a4\ud2b8 \ubcc0\ud658\uae30", None))
         self.label.setText(QCoreApplication.translate("Form", u"\uc6d0\ubcf8", None))
         self.label_status.setText("")
-        self.plainTextEdit.setPlainText("")
+        self.plainTextEdit_source.setPlainText("")
         self.groupBox_2.setTitle(QCoreApplication.translate("Form", u"\uacb0\uacfc \ucc98\ub9ac", None))
 #if QT_CONFIG(tooltip)
         self.pushButton_clipboard.setToolTip(QCoreApplication.translate("Form", u"\uacb0\uacfc \ub0b4\uc6a9\uc744 \ud074\ub9bd\ubcf4\ub4dc\ub85c \ubcf5\uc0ac\ud569\ub2c8\ub2e4.", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButton_clipboard.setText(QCoreApplication.translate("Form", u"\ud074\ub9bd\ubcf4\ub4dc\ub85c \ubcf5\uc0ac", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"\uc55e \ucd94\uac00:", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"\ub4a4 \ucd94\uac00:", None))
+        self.pushButton_add.setText(QCoreApplication.translate("Form", u"\uc55e/\ub4a4 \ucd94\uac00", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"\uacb0\uacfc", None))
         self.groupBox.setTitle(QCoreApplication.translate("Form", u"\ubcc0\ud658 \uc791\uc5c5", None))
-#if QT_CONFIG(tooltip)
-        self.checkBox_tab.setToolTip(QCoreApplication.translate("Form", u"\ud0ed \uad6c\ubd84\uc744 \ub123\uac70\ub098 \ube8d\ub2c8\ub2e4.", None))
-#endif // QT_CONFIG(tooltip)
-        self.checkBox_tab.setText(QCoreApplication.translate("Form", u"\ud0ed \uad6c\ubd84", None))
 #if QT_CONFIG(tooltip)
         self.pushButton_white_space.setToolTip(QCoreApplication.translate("Form", u"\ud0ed, \uc0c8 \uc904 \ubb38\uc790 \ub4f1\uc744 \\t, \\n \ub4f1\uc73c\ub85c \ubc14\uafc9\ub2c8\ub2e4.", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButton_white_space.setText(QCoreApplication.translate("Form", u"\uacf5\ubc31 \ubb38\uc790", None))
 #if QT_CONFIG(tooltip)
+        self.pushButton_unicode.setToolTip(QCoreApplication.translate("Form", u"\uc720\ub2c8\ucf54\ub4dc \uc774\uc2a4\ucf00\uc774\ud504 \uc2dc\ud000\uc2a4(\uc608: u00e9)\ub97c \uc0ac\ub78c\uc774 \uc77d\uc744 \uc218 \uc788\ub294 \ubb38\uc790\ub85c \ubcc0\ud658\ud569\ub2c8\ub2e4.", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_unicode.setText(QCoreApplication.translate("Form", u"\uc720\ub2c8\ucf54\ub4dc", None))
+#if QT_CONFIG(tooltip)
+        self.checkBox_tab.setToolTip(QCoreApplication.translate("Form", u"\ud0ed \uad6c\ubd84\uc744 \ub123\uac70\ub098 \ube8d\ub2c8\ub2e4.", None))
+#endif // QT_CONFIG(tooltip)
+        self.checkBox_tab.setText(QCoreApplication.translate("Form", u"\ud0ed \uad6c\ubd84", None))
+#if QT_CONFIG(tooltip)
         self.pushButton_dict.setToolTip(QCoreApplication.translate("Form", u"\uc5ec\ub7ec \uc904\ub85c \uad6c\ubd84\ub418\uc5b4 \uc788\ub294 \ubaa9\ub85d\uc744 \ud30c\uc774\uc36c\uc758 \ub515\uc154\ub108\ub9ac \ud615\uc2dd\uc73c\ub85c \ubcc0\ud658\ud569\ub2c8\ub2e4.", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButton_dict.setText(QCoreApplication.translate("Form", u"\ub515\uc154\ub108\ub9ac", None))
 #if QT_CONFIG(tooltip)
-        self.pushButton_clear.setToolTip(QCoreApplication.translate("Form", u"\uc6d0\ubcf8 \ub0b4\uc6a9\uc744 \uc9c0\uc6c1\ub2c8\ub2e4.", None))
+        self.checkBox_number.setToolTip(QCoreApplication.translate("Form", u"\uad6c\ud604\ub418\uc9c0 \uc54a\uc74c", None))
 #endif // QT_CONFIG(tooltip)
-        self.pushButton_clear.setText(QCoreApplication.translate("Form", u"\uc9c0\uc6b0\uae30", None))
-#if QT_CONFIG(tooltip)
-        self.pushButton_unicode.setToolTip(QCoreApplication.translate("Form", u"\uc720\ub2c8\ucf54\ub4dc \uc774\uc2a4\ucf00\uc774\ud504 \uc2dc\ud000\uc2a4(\uc608: u00e9)\ub97c \uc0ac\ub78c\uc774 \uc77d\uc744 \uc218 \uc788\ub294 \ubb38\uc790\ub85c \ubcc0\ud658\ud569\ub2c8\ub2e4.", None))
-#endif // QT_CONFIG(tooltip)
-        self.pushButton_unicode.setText(QCoreApplication.translate("Form", u"\uc720\ub2c8\ucf54\ub4dc \uc2dc\ud000\uc2a4", None))
+        self.checkBox_number.setText(QCoreApplication.translate("Form", u"\uc22b\uc790 \uc720\uc9c0", None))
 #if QT_CONFIG(tooltip)
         self.pushButton_list.setToolTip(QCoreApplication.translate("Form", u"\uc5ec\ub7ec \uc904\ub85c \uad6c\ubd84\ub418\uc5b4 \uc788\ub294 \ubaa9\ub85d\uc744 \ud30c\uc774\uc36c\uc758 \ub9ac\uc2a4\ud2b8 \ud615\uc2dd\uc73c\ub85c \ubcc0\ud658\ud569\ub2c8\ub2e4.", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButton_list.setText(QCoreApplication.translate("Form", u"\ub9ac\uc2a4\ud2b8", None))
 #if QT_CONFIG(tooltip)
-        self.checkBox_number.setToolTip(QCoreApplication.translate("Form", u"\uad6c\ud604\ub418\uc9c0 \uc54a\uc74c", None))
+        self.pushButton_prod_code.setToolTip(QCoreApplication.translate("Form", u"\uac01 \uc904\ubcc4\ub85c \ud488\ubc88(\uc608: JUL-001) \ucd94\ucd9c", None))
 #endif // QT_CONFIG(tooltip)
-        self.checkBox_number.setText(QCoreApplication.translate("Form", u"\uc22b\uc790 \uc720\uc9c0", None))
+        self.pushButton_prod_code.setText(QCoreApplication.translate("Form", u"\ud488\ubc88 \ucd94\ucd9c", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_clear.setToolTip(QCoreApplication.translate("Form", u"\uc6d0\ubcf8 \ub0b4\uc6a9\uc744 \uc9c0\uc6c1\ub2c8\ub2e4.", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_clear.setText(QCoreApplication.translate("Form", u"\uc9c0\uc6b0\uae30", None))
     # retranslateUi
 
